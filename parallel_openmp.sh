1a38d210a739c969d69f72406bb7de1d82120754
Jonathan Frawley <jmf@9lines.org>
2020-07-10 16:54:17 +0100

    Add parallel_openmp.sh script

--- parallel_openmp.sh
+++ parallel_openmp.sh
@@ -14,8 +14,8 @@ export CPLUS_INCLUDE_PATH="/usr/include/vtk/:$CPLUS_INCLUDE_PATH"
 
 export OMP_NUM_THREADS=1
 
-#./configure --enable-exahype --with-multithreading=omp CFLAGS="-fno-devirtualize" CXXFLAGS="-fno-devirtualize" LDFLAGS="-fno-devirtualize"
-./configure --enable-exahype --with-multithreading=omp CFLAGS="-fno-devirtualize -foffload=-lm -fno-fast-math -fno-associative-math" CXXFLAGS="-fno-devirtualize -foffload=-lm -fno-fast-math -fno-associative-math" LDFLAGS="-fno-devirtualize" # https://stackoverflow.com/a/59164967
+./configure --with-vtk --with-vtk-version=8 --with-vtk-suffix='' --enable-exahype --with-multithreading=omp CFLAGS="-fno-devirtualize -foffload=-lm -fno-fast-math -fno-associative-math" CXXFLAGS="-fno-devirtualize -foffload=-lm -fno-fast-math -fno-associative-math" LDFLAGS="-fno-devirtualize" # https://stackoverflow.com/a/59164967
+
 make -j 8
 pushd python/examples/exahype2/euler/
 export PYTHONPATH=../../../
